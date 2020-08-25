4bd402050252503ade0d10d4af8b7f791168b100
Carson Black <uhhadd@gmail.com>
2020-08-24 23:10:07 -0400

    build

--- docs/js/main.js
+++ docs/js/main.js
@@ -13,19 +13,41 @@ document.querySelector(".nlp-scrim").addEventListener("scroll", function (e) {
     CloseDrawer();
   }
 });
+var flippedClass = "nlp-flipcard--flipped";
+var cards = document.querySelectorAll(".nlp-flipcard");
+cards.forEach(function (element) {
+  element.addEventListener("click", function (e) {
+    if (element.classList.contains(flippedClass)) {
+      element.classList.remove(flippedClass);
+    } else {
+      element.classList.add(flippedClass);
+    }
+  });
+});
+
+function sleep(ms) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, ms);
+  });
+}
 
 function OpenDrawer() {
   var drawer = document.querySelector(".nlp-paned__left");
   var scrim = document.querySelector(".nlp-scrim");
   scrim.appendChild(drawer);
-  drawer.classList.add("nlp-paned__left--open");
   scrim.classList.add("nlp-scrim--open");
-  scrim.scrollTo(scrim.scrollHeight, 0);
+  setTimeout(function () {
+    drawer.classList.add("nlp-paned__left--reparented");
+    drawer.classList.add("nlp-paned__left--open");
+  }, 150);
+  var padding = document.querySelector(".nlp-scrim__padder");
+  scrim.scrollTo(padding.scrollHeight / 2, 0);
 }
 
 function CloseDrawer() {
   var drawer = document.querySelector(".nlp-paned__left");
   drawer.classList.remove("nlp-paned__left--open");
+  drawer.classList.remove("nlp-paned__left--reparented");
   var scrim = document.querySelector(".nlp-scrim");
   scrim.classList.remove("nlp-scrim--open");
   document.querySelector(".nlp-paned").prepend(drawer);
